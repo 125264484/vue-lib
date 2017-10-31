9f849aa9b556e5bea97531141bd9bef93f6b6193
125264484 <125264484@qq.com>
2017-10-31 15:27:42 +0800

    add npm ignore

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter);
+import '../packages/theme/date-picker.scss'
 const routes = [
 
     {
--- packages/date-picker/src/main.vue
+++ packages/date-picker/src/main.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>{{msg}}</div>
+    <div class="Test">{{msg}}</div>
 </template>
 <script>
     export default {
